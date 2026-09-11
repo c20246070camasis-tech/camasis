b1ceb344e10399a9c742d37f69f47b433c674345
c20246070camasis-tech <c20246070.camasis@csav.edu.ph>
2026-09-11 09:22:35 +0800

    variables (let/const)

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello world.");
+console.log('Hello world.');
+const Myname = "CJ H CAMASIS";
+console.log('NAME: ' + Myname);
+let age = "20";
+console.log('AGE: ' + age);
+let number = "09123456789";
+console.log('NUMBER: ' + number);
+let address = "Barangay X Estado, victorias City";
+console.log('ADDRESS: ' + address);
