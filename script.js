37515c507c113af510006b1bce63bed5afe8036f
c20246070camasis-tech <c20246070.camasis@csav.edu.ph>
2026-09-11 12:20:18 +0800

    addEventListener

--- script.js
+++ script.js
@@ -53,3 +53,26 @@ heading.style.backgroundColor = "yellow";
 contactHeading.style.color = "purple";
 serviceHeading.style.color = "purple";
 projectHeading.style.color = "purple";
+
+//event listener
+heading.addEventListener("click", function () {
+    heading.style.color = "red";
+
+});
+
+const toggleButton = document.querySelector('#switch');
+const body = document.querySelector('body');
+let isOff = false;
+
+toggleButton.addEventListener("click", function () {
+    isOff = !isOff;
+
+    if (isOff) {
+        body.style.backgroundColor = "black";
+        body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+
+    }
+});
