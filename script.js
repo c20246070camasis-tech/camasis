639e3ed24d124ef4c327c23a9cc292084ac8aa9c
c20246070camasis-tech <c20246070.camasis@csav.edu.ph>
2026-09-11 11:54:07 +0800

    .style

--- script.js
+++ script.js
@@ -29,25 +29,27 @@ console.log(mdas(5, 3))
 //query selector
 
 const heading = document.querySelector("h1")
-
 console.log(heading);
 
-
 const contactHeading = document.querySelector("#contact h2");
-
 console.log(contactHeading);
 
+const serviceHeading = document.querySelector("#services h2");
+console.log(serviceHeading);
 
-const projectHeading = document.querySelector("#services h2");
-
+const projectHeading = document.querySelector("#projects h2");
 console.log(projectHeading);
-
 
 //Text Context
 
 heading.textContent = "My portfolio";
-
 contactHeading.textContent = "Let's Connect!";
-
+serviceHeading.textContent = "My Services";
 projectHeading.textContent = "My Projects";
 
+//.style
+heading.style.color = "purple";
+heading.style.backgroundColor = "yellow";
+contactHeading.style.color = "purple";
+serviceHeading.style.color = "purple";
+projectHeading.style.color = "purple";
