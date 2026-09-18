e4e58e9dc146e9301578d5e9e7b71af80234f308
c20246070camasis-tech <c20246070.camasis@csav.edu.ph>
2026-09-18 11:14:09 +0800

    firts web

--- script.js
+++ script.js
@@ -1,31 +1,5 @@
 
-console.log('Hello world.');
-const Myname = "CJ H CAMASIS";
-console.log('NAME: ' + Myname);
-let age = "20";
-console.log('AGE: ' + age);
-let number = "09123456789";
-console.log('NUMBER: ' + number);
-let address = "Barangay X Estado, victorias City";
-console.log('ADDRESS: ' + address);
 
-function greet(name) {
-    return `Good Morning ${name}!`;
-}
-console.log(greet("Rene"));
-
-
-
-
-function mdas(num1, num2) {
-    let m = num1 * num2;
-    let d = num1 / num2;
-    let a = num1 + num2;
-    let sub = num1 - num2;
-
-    return (`values: ${num1} and ${num2}.\n product ${m},\n quotient ${d}, \n sum ${a},\n difference ${sub}`);
-}
-console.log(mdas(5, 3))
 
 //query selector
 
